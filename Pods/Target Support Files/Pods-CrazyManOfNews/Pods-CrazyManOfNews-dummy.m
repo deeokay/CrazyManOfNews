f38fa05acda0c4f6c29d13f4dcc77cb7ec32664d
deeokay <qianjiehao@qq.com>
2016-12-02 15:17:51 +0800

    hey

---
+++ Pods/Target Support Files/Pods-CrazyManOfNews/Pods-CrazyManOfNews-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CrazyManOfNews : NSObject
+@end
+@implementation PodsDummy_Pods_CrazyManOfNews
+@end
---
+++ Pods/Target Support Files/UIImage+animatedGif/UIImage+animatedGif-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "UIImage+animatedGIF.h"
+
+FOUNDATION_EXPORT double UIImage_animatedGifVersionNumber;
+FOUNDATION_EXPORT const unsigned char UIImage_animatedGifVersionString[];
+
